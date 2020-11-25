b357fa30b898862451fb23f2df5579a45c3ff1ad
KaveeshaKularathne <kaveesha@virtuan.io>
2020-11-25 21:46:23 +0530

    Adding Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormControlDirective } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import{DashboardComponent}from './dashboard/dashboard.component';
+import{HeroesComponent}from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  {path:'heroes',component:HeroesComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+ 
+
 })
 export class AppRoutingModule { }
